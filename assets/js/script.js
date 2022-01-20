e3cf9cb88a0dc5b48594ec7ef8343575cb332d63
LoftusN13 <loftus.niamh@yahoo.ie>
2022-01-20 17:54:47 +0000

    Get clock's seconds hand moving

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
+const secondHand = document.querySelector(".second-hand");
+
 function setDate() {
     const now = new Date();
+    
     const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360);
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
     console.log(seconds);
 }
 
